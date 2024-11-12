84006f7d79faf37409907d5eae574f4f07d158a0
Sebastian Nomme <sebastian.nomme@sparebank1.no>
2024-11-12 09:14:15 +0100

    bypass node_modules warnings

--- src/main.tsx
+++ src/main.tsx
@@ -25,7 +25,14 @@ async function enableMocking() {
 
   // `worker.start()` returns a Promise that resolves
   // once the Service Worker is up and ready to intercept requests.
-  return worker.start();
+  return worker.start({
+    onUnhandledRequest: (req: Request) => {
+      if (req.url.includes("/node_modules/")) {
+        // Bypass any requests to node_modules
+        return "bypass";
+      }
+    },
+  });
 }
 
 const rootElement = document.getElementById("app")!;
